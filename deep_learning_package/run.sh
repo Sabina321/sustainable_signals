503d0e50db485c6c340b951c7c46250ef9985b7c
Tianliang Xu <xutianliang128@gmail.com>
2023-05-24 14:18:37 -0400

    Change run.sh

--- deep_learning_package/run.sh
+++ deep_learning_package/run.sh
@@ -1,33 +1,71 @@
-for mt in climatebert/distilroberta-base-climate-f 
-do
-    for hou_dim in 512 768
-    do
-        for bea_dim in 512 768
-        do
-            for bb_dim in 512 768
-            do
-                for kit_dim in 512 768
-                do
+python3 main.py \
+    --num_epochs 13 \
+    --mode onTest \
+    --model_type Cate \
+    --model_name distilbert-base-uncased \
+    --train_batch_size 32 \
+    --test_batch_size 32 \
+    --save_logging_steps 500 \
+    --learning_rate 5e-5 \
+    --house_dim 512 \
+    --beauty_dim 512 \
+    --baby_dim 512 \
+    --kitchen_dim 768 \
 
-                python3 main.py \
-                        --num_epochs 13 \
-                        --mode onTest \
-                        --train_batch_size 32 \
-                        --test_batch_size 32 \
-                        --model_type Cate \
-                        --model_name $mt \
-                        --save_logging_steps 500 \
-                        --learning_rate 5e-5 \
-                        --house_dim $hou_dim \
-                        --beauty_dim $bea_dim \
-                        --baby_dim $bb_dim \
-                        --kitchen_dim $kit_dim \
+python3 main.py \
+    --num_epochs 13 \
+    --mode onTest \
+    --model_type Cate \
+    --model_name distilroberta-base \
+    --train_batch_size 32 \
+    --test_batch_size 32 \
+    --save_logging_steps 500 \
+    --learning_rate 5e-5 \
+    --house_dim 768 \
+    --beauty_dim 512 \
+    --baby_dim 768 \
+    --kitchen_dim 768 \
 
-                done
-            done
-        done
-    done
-done
+python3 main.py \
+    --num_epochs 13 \
+    --mode onTest \
+    --model_type Cate \
+    --model_name climatebert/distilroberta-base-climate-f \
+    --train_batch_size 32 \
+    --test_batch_size 32 \
+    --save_logging_steps 500 \
+    --learning_rate 5e-5 \
+    --house_dim 512 \
+    --beauty_dim 768 \
+    --baby_dim 768 \
+    --kitchen_dim 512 \
 
+python3 main.py \
+    --num_epochs 13 \
+    --mode onTest \
+    --model_type noCate \
+    --model_name distilbert-base-uncased \
+    --train_batch_size 32 \
+    --test_batch_size 32 \
+    --save_logging_steps 500 \
+    --learning_rate 5e-5 \
 
+python3 main.py \
+    --num_epochs 13 \
+    --mode onTest \
+    --model_type noCate \
+    --model_name distilroberta-base \
+    --train_batch_size 32 \
+    --test_batch_size 32 \
+    --save_logging_steps 500 \
+    --learning_rate 5e-5 \
 
+python3 main.py \
+    --num_epochs 13 \
+    --mode onTest \
+    --model_type noCate \
+    --model_name climatebert/distilroberta-base-climate-f \
+    --train_batch_size 32 \
+    --test_batch_size 32 \
+    --save_logging_steps 500 \
+    --learning_rate 5e-5 \
